dce36dc817a71e290f2838fc7c3fc4b8e1907bbd
Pakyudae . <visitacionmaquee@gmail.com>
2026-06-23 16:32:20 +0800

    Integrated a Login system and Initialize the database using Supabase

---
+++ habit-tracker-cicd/src/supabaseClient.js
@@ -0,0 +1,7 @@
+import { createClient } from '@supabase/supabase-js';
+
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+
+// We pass the variables here instead of the raw text strings
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
